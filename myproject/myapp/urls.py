8abebc7e70fb3d4eaba4345183e74a38509839ec
Svetlanakrasyuk <cvmilplus@gmail.com>
2023-11-06 17:53:13 +0700

    cache in html template

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,5 +4,6 @@ from django.views.decorators.cache import cache_page
 from .views import ShowHome
 
 urlpatterns = [
-    path("", cache_page(60 * 1)(ShowHome.as_view()), name="home"),
+    path("", ShowHome.as_view(), name="home"),
+    # path("", cache_page(60 * 1)(ShowHome.as_view()), name="home"),
 ]
